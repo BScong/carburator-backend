15c90208e2f259e33b56793d754dc1dc4b0621e2
bscong <benoit.zhong@gmail.com>
2017-11-15 14:30:46 +0100

    Adding values to MongoDB database

--- populate.py
+++ populate.py
@@ -1,10 +1,19 @@
 import xml.etree.ElementTree as ET
 import json
+from pymongo import MongoClient
 
+print("Parsing XML data...")
 tree = ET.parse('sample.xml')
 root = tree.getroot()
-types={}
-count=0
+
+print("Connecting to database...")
+client = MongoClient("mongodb://localhost:27017")
+db = client['carburator']
+
+to_retry = []
+
+count,total=0,0
+print("Adding values...")
 for child in root:
 	data = {
 		'id':child.attrib['id'],
@@ -39,6 +48,23 @@ for child in root:
 		prices[price.attrib['id']] = priceData
 	data['prices']=prices
 
-	count+=1
-	print(json.dumps(data, sort_keys=True, indent=4))
-print(count)
+	result = db.stations.insert_one(data)
+
+	if not result.acknowledged:
+		to_retry.append(data) 
+	else:
+		count+=1
+	total+=1
+	#print(json.dumps(data, sort_keys=True, indent=4))
+
+# Retry one time if error occured
+for data in to_retry:
+	result = db.stations.insert_one(data)
+	if not result.acknowledged:
+		print("Error adding id " + data['id'] + " to the database")
+	else:
+		count+=1
+
+client.close()
+
+print("Finished adding values, total " + str(total) + " elements, "+ str(count) + " added.")
